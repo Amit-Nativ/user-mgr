217f6247a8d26f3f4dc6338906229157c7490e71
Amit Nativ <amitnativ11@gmail.com>
2020-10-20 10:46:13 +0300

    wip express

--- server/config/express/index.js
+++ server/config/express/index.js
@@ -20,12 +20,9 @@ export default () => {
 
   app.use(cors())
   app.use(helmet());
-  app.use(bodyparser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
-  app.use(bodyparser.json({ limit: '50mb' }));
-  app.use(methodOverride());
-  app.use(cookieParser());
-  app.use(passport.initialize());
-  app.use(passport.session());
+  app.use(bodyparser.urlencoded({ extended: false }));
+  app.use(bodyparser.json());
+  // app.use(methodOverride());
 
   app.use(express.static(join(__dirname, "../../../client")));
   // app.use(staticGzip( '../../../client'));
